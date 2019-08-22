f7fc47eb6ea080d0c5534c9133c85d67dff835ef
vishalpaysense <vishal.gupta@Paysenses-MacBook-Pro-4.local>
2019-08-22 23:15:05 +0530

    22Aug

---
+++ first_last_occurence.py
@@ -0,0 +1,22 @@
+class Solution: 
+  def getRange(self, arr, target):
+    len_arr = len(arr)
+    first = last = -1
+    for i in range(0, len_arr):
+    	if (arr[i] == target) & (first == -1):
+    		first = i
+
+    	if (arr[len_arr-i-1] == target)  &  (last == -1):
+    		last = len_arr-i-1
+
+    return [first, last]
+  
+# Test program 
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
+x = 2
+print(Solution().getRange(arr, x))
+
+
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
+x = 9
+print(Solution().getRange(arr, x))
